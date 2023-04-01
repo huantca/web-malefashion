ca40919cd822e2174ba3df7a2b8235c95179d812
Anh <maianhxtb@gmail.com>
2023-04-01 12:59:08 +0700

    Manh

--- webMalefashion/Controllers/HomeController.cs
+++ webMalefashion/Controllers/HomeController.cs
@@ -1,21 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using System.Diagnostics;
 using webMalefashion.Models;
+using X.PagedList;
 
 namespace webMalefashion.Controllers
 {
     public class HomeController : Controller
     {
+        // MalefashionContext 
+        MalefashionContext db = new MalefashionContext();
+       
+        private string? pagedlst;
+        private int page;
         private readonly ILogger<HomeController> _logger;
+        // private object db;
 
         public HomeController(ILogger<HomeController> logger)
         {
+            
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? page)       
         {
+            int pageSize = 4;// số sản phẩm trên 1 trang
+            int pageNumber = page == null || page < 1 ? 1 : page.Value;
+
+            var lstsanpham = db.Products.AsNoTracking().OrderBy(x => x.Name);
+            PagedList<Product> lst = new PagedList<Product>(lstsanpham, pageNumber, pageSize);
+
+            return View(lst);
+        }
+            public IActionResult ShoppingCart()
+        {
+
             return View();
+        }
+        public IActionResult SPMenu(int? page)
+            
+        {
+            int pageSize = 4;// số sản phẩm trên 1 trang
+            int pageNumber = page == null || page < 1 ? 1 : page.Value;
+
+            var lstsanpham = db.Products.AsNoTracking().OrderBy(x => x.Name);
+            PagedList<Product> lst = new PagedList<Product>(lstsanpham, pageNumber, pageSize);
+
+            return View(lst);
+          
         }
 
         public IActionResult Privacy()
---
+++ webMalefashion/Controllers/MaleFashionContext.cs
@@ -0,0 +1,13 @@
+﻿namespace webMalefashion.Controllers
+{
+    internal class MaleFashionContext
+    {
+        public MaleFashionContext()
+        {
+        }
+
+        public object TProducts { get; internal set; }
+        public object TProduct { get; internal set; }
+        public object Products { get; internal set; }
+    }
+}

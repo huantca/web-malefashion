266298e5e57c8e2be26bfae4f9ad5f161cd5142b
Anh <maianhxtb@gmail.com>
2023-04-03 23:05:32 +0700

    BuiltMenu

---
+++ webMalefashion/ViewComponents/LoaiColorMenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webMalefashion.Responsitory;
+
+namespace webMalefashion.ViewComponents
+{
+    public class LoaiColorMenuViewComponent: ViewComponent
+    {
+        private readonly ILoaiBrandResponsitory _loaiColor;
+        public LoaiColorMenuViewComponent(ILoaiBrandResponsitory loaiBrandResponsitory)
+        {
+            _loaiColor = loaiBrandResponsitory;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var loaicolor = _loaiColor.GetAllLoaiColor().OrderBy(x => x.SizeId);
+            //
+
+
+            return View(loaicolor);
+
+        }
+
+
+
+
+    }
+}
